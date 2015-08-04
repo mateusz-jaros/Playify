408410779774e7687bf4d76a6085c64e364933c1
Mateusz Jaros <jarosmatt@gmail.com>
2015-08-04 13:03:09 +0200

    Initial Commit

---
+++ Playify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Playify
+//
+//  Created by Mateusz Jaros on 04.08.2015.
+//  Copyright (c) 2015 Mateusz Jaros. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Playify/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Playify
+//
+//  Created by Mateusz Jaros on 04.08.2015.
+//  Copyright (c) 2015 Mateusz Jaros. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
